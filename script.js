7ec75f9664ee5b85a79af0d46a58f7364289a5b7
Adam <adamsaul314@gmail.com>
2025-01-12 09:47:32 +0000

    style added for flexbox 16x16 grid

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // document.getElementById('container').innerHTML = parentDiv
 
 // let parentDiv
-const container = document.querySelector('#container')
+const container = document.querySelector('.container')
 let divNum = 1
 
-for (let i =0; i < 16; i++){
+for (let i =0; i < 256; i++){
     
 const content = document.createElement('div')
-content.classList.add(`content${divNum}`)
+content.classList.add(`content${divNum}`, 'content')
 container.appendChild(content)
 divNum ++
 }
